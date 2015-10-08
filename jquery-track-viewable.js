65d0d768940e7d0cc7f670cbfeacbc624ede7268
Horace <Horace@macbook.local>
2015-10-07 23:38:36 -0400

    Bug fixes and such.

--- jquery-track-viewable.js
+++ jquery-track-viewable.js
@@ -8,49 +8,54 @@
  * @returns {Boolean} Whether the element has been viewable.
  */
 $.fn.trackViewable = function(options, next) {
-    var $e = $(this);
-    $(options.scope || window).scroll(function() {
-        var result,
-            mA = options.minArea || 0,
-            mW = options.minWidth || 0,
-            mH = options.minHeight || 0,
-            wW = $(this).width(),
-            wH = $(this).height(),
-            sT = $(this).scrollTop(),
-            sL = $(this).scrollLeft(),
-            sB = wH + sT,
-            sR = wW + sL,
-            eT = $e.offset().top,
-            eL = $e.offset().left,
-            eH = $e.height(),
-            eW = $e.width(),
-            eA = eH * eW,
-            vW = sL < eL ? Math.min(Math.max(sR - eL, 0), eW) : Math.max(eW - (sL - eL), 0),
-            vWP = Math.round(vW / eW * 1000) / 10,
-            vH = sT < eT ? Math.min(Math.max(sB - eT, 0), eH) : Math.max(eH - (sT - eT), 0),
-            vHP = Math.round(vH / eH * 1000) / 10,
-            vA = vW * vH,
-            vAP = Math.round(vA / eA * 1000) / 10,
-            isViewable = vAP > mA && vWP > mW && vHP > mH
+    var VIEW_START_ATTR = "data-view-start",
+        VIEWED_ATTR = "data-viewed"
+    $(this).each(function(index, item) {
+        (function($e) {
+            if ($e.hasClass("viewed")) return;
 
-        result = {
-            isViewable: isViewable,
-            viewableArea: vA,
-            viewableAreaPercentage: vAP,
-            viewableWidth: vW,
-            viewableWidthPercentage: vWP,
-            viewableHeight: vH,
-            viewableHeightPercentage: vHP,
-            $element: $e,
-            options: options
-        };
+            $(options.scope || window).scroll(function() {
+                var result,
+                    mA = options.minArea || 0,
+                    mW = options.minWidth || 0,
+                    mH = options.minHeight || 0,
+                    wW = $(this).width(),
+                    wH = $(this).height(),
+                    sT = $(this).scrollTop(),
+                    sL = $(this).scrollLeft(),
+                    sB = wH + sT,
+                    sR = wW + sL,
+                    eT = $e.offset().top,
+                    eL = $e.offset().left,
+                    eH = $e.height(),
+                    eW = $e.width(),
+                    eA = eH * eW,
+                    vW = sL < eL ? Math.min(Math.max(sR - eL, 0), eW) : Math.max(eW - (sL - eL), 0),
+                    vWP = Math.round(vW / eW * 1000) / 10,
+                    vH = sT < eT ? Math.min(Math.max(sB - eT, 0), eH) : Math.max(eH - (sT - eT), 0),
+                    vHP = Math.round(vH / eH * 1000) / 10,
+                    vA = vW * vH,
+                    vAP = Math.round(vA / eA * 1000) / 10,
+                    isViewable = vAP > mA && vWP > mW && vHP > mH;
 
-        if (typeof next === "function") {
-            next(result);
-        }
+                result = {
+                    isViewable: isViewable,
+                    viewableArea: vA,
+                    viewableAreaPercentage: vAP,
+                    viewableWidth: vW,
+                    viewableWidthPercentage: vWP,
+                    viewableHeight: vH,
+                    viewableHeightPercentage: vHP,
+                    $element: $e,
+                    options: options
+                };
 
-        return result;
+                if (typeof next === "function") {
+                    next(result);
+                }
+
+                return;
+            });
+        })($(item));
     });
-
-    return this;
 };
