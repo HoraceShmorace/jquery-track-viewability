a1e1d2b1ff9f90d864a71bf7ac3e0ddcacfa97de
Horace Nelson <horaceshmorace@gmail.com>
2015-10-09 14:03:34 -0400

    Update jquery-viewable.js

--- jquery-viewable.js
+++ jquery-viewable.js
@@ -14,42 +14,42 @@ $.fn.viewable = function(next, scope) {
 
     if (!scope) scope = window;
 
+    var calculateHowViewable = function() {
+        var result,
+            wW = $(this).width(),
+            wH = $(this).height(),
+            sT = $(this).scrollTop(),
+            sL = $(this).scrollLeft(),
+            sB = wH + sT,
+            sR = wW + sL,
+            eT = $e.offset().top,
+            eL = $e.offset().left,
+            eH = $e.height(),
+            eW = $e.width(),
+            eA = eH * eW,
+            vW = sL < eL ? Math.min(Math.max(sR - eL, 0), eW) : Math.max(eW - (sL - eL), 0),
+            vWP = Math.round(vW / eW * 1000) / 10,
+            vH = sT < eT ? Math.min(Math.max(sB - eT, 0), eH) : Math.max(eH - (sT - eT), 0),
+            vHP = Math.round(vH / eH * 1000) / 10,
+            vA = vW * vH,
+            vAP = Math.round(vA / eA * 1000) / 10;
+
+        result = {
+            viewableArea: vA,
+            viewableAreaPercentage: vAP,
+            viewableWidth: vW,
+            viewableWidthPercentage: vWP,
+            viewableHeight: vH,
+            viewableHeightPercentage: vHP,
+            $element: $e,
+            scope: scope
+        };
+
+        next(result);
+    }
+
     $(this).each(function(index, item) {
         (function($e) {
-            var calculateHowViewable = function() {
-                var result,
-                    wW = $(this).width(),
-                    wH = $(this).height(),
-                    sT = $(this).scrollTop(),
-                    sL = $(this).scrollLeft(),
-                    sB = wH + sT,
-                    sR = wW + sL,
-                    eT = $e.offset().top,
-                    eL = $e.offset().left,
-                    eH = $e.height(),
-                    eW = $e.width(),
-                    eA = eH * eW,
-                    vW = sL < eL ? Math.min(Math.max(sR - eL, 0), eW) : Math.max(eW - (sL - eL), 0),
-                    vWP = Math.round(vW / eW * 1000) / 10,
-                    vH = sT < eT ? Math.min(Math.max(sB - eT, 0), eH) : Math.max(eH - (sT - eT), 0),
-                    vHP = Math.round(vH / eH * 1000) / 10,
-                    vA = vW * vH,
-                    vAP = Math.round(vA / eA * 1000) / 10;
-
-                result = {
-                    viewableArea: vA,
-                    viewableAreaPercentage: vAP,
-                    viewableWidth: vW,
-                    viewableWidthPercentage: vWP,
-                    viewableHeight: vH,
-                    viewableHeightPercentage: vHP,
-                    $element: $e,
-                    scope: scope
-                };
-
-                next(result);
-            }
-
             $(scope).scroll(calculateHowViewable);
             $(window).load(calculateHowViewable);
 
